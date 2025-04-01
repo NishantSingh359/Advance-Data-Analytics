21da4c346a1f3e8fa06751b3083a7fca34c85a8f
Nishant Singh <singhnishant414243@gmail.com>
2025-04-01 07:22:07 -0700

    Update Change_Over-Time (Trendes).sql

--- script/Change_Over-Time (Trendes).sql
+++ script/Change_Over-Time (Trendes).sql
@@ -1,1 +1,36 @@
 
+-- =================== Changes Over Time Analysis ===================
+   
+-- Total Sales, Customer, Quantity by Year
+SELECT 
+YEAR(order_date) AS years,
+SUM(sales_amount) AS total_sales,
+COUNT( DISTINCT customer_key) AS total_customer,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)
+ORDER BY years ASC;
+
+-- Total Sales, Customer, Quantity for each Year and Month
+SELECT 
+YEAR(order_date) AS years,
+MONTH(order_date) months,
+SUM(sales_amount) AS total_sales,
+COUNT( DISTINCT customer_key) AS total_customer,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date), MONTH(order_date)
+ORDER BY years ASC;
+
+-- Total Sales, Customer, Quantity by Month
+SELECT 
+MONTH(order_date) months, 
+SUM(sales_amount) AS total_sales,
+COUNT( DISTINCT customer_key) AS total_customer,
+SUM(quantity) AS total_quantity
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY  MONTH(order_date)
+ORDER BY months ASC;
