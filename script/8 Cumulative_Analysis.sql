e02889b62d1f4ed8c414b06c9af4fe44e816b360
Nishant Singh <singhnishant414243@gmail.com>
2025-04-03 01:37:03 -0700

    Update 8 Cumulative_Analysis.sql

--- script/8 Cumulative_Analysis.sql
+++ script/8 Cumulative_Analysis.sql
@@ -1,1 +1,35 @@
+-- =================== Cumilative Analysis ===================
+
+-- Total running sales by years
+SELECT 
+years,
+total_sales,
+SUM(total_sales) OVER( ORDER BY years, months) AS running_total_sales
+FROM (
+SELECT 
+YEAR(order_date) years, 
+SUM(sales_amount) AS total_sales
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)  , MONTH(order_date) 
+) AS t
+
+-- Total running sales and quantity with Each Months by Year
+SELECT 
+years,
+months,
+total_sales,
+SUM(total_sales) OVER(PARTITION BY years ORDER BY years, months) AS running_total_sales,
+avg_price,
+AVG(avg_price) OVER(PARTITION BY years ORDER BY years, months) AS running_avg_price
+FROM (
+SELECT 
+YEAR(order_date) years,
+MONTH(order_date) months, 
+SUM(sales_amount) AS total_sales,
+AVG(price) AS avg_price
+FROM gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY YEAR(order_date)  , MONTH(order_date) 
+) AS t
 
