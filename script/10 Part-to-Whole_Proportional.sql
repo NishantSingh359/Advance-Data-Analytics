b5caa3ae39707924e2182556a773b34bf01be3a3
Nishant Singh <singhnishant414243@gmail.com>
2025-04-03 01:52:34 -0700

    Update 10 Part-to-Whole_Proportional.sql

--- script/10 Part-to-Whole_Proportional.sql
+++ script/10 Part-to-Whole_Proportional.sql
@@ -1,1 +1,21 @@
+-- =================== Part-to-Whole ===================
+
+-- Which categories contribute the most to overall sales?
+
+WITH cate_contribute AS (
+SELECT 
+  category,
+  SUM(sales_amount) as total_sales
+FROM gold.fact_sales AS s
+left join gold.dim_products AS p
+ON s.product_key = p.product_key
+GROUP BY category
+)
+SELECT
+  category,
+  total_sales,
+  SUM(total_sales) OVER() AS overall_sales,
+  CONCAT(ROUND((CAST(total_sales AS FLOAT)/SUM(total_sales) OVER())*100,2),'%') AS percentage_of_sales  
+FROM cate_contribute
+ORDER BY total_sales DESC;
 
