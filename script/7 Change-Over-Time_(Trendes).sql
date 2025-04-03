c32efc5b027db7ba38697706e1300f9e1c5808f4
Nishant Singh <singhnishant414243@gmail.com>
2025-04-03 01:48:06 -0700

    Update 7 Change-Over-Time_(Trendes).sql

--- script/7 Change-Over-Time_(Trendes).sql
+++ script/7 Change-Over-Time_(Trendes).sql
@@ -3,10 +3,10 @@
    
 -- Total Sales, Customer, Quantity by Year
 SELECT 
-YEAR(order_date) AS years,
-SUM(sales_amount) AS total_sales,
-COUNT( DISTINCT customer_key) AS total_customer,
-SUM(quantity) AS total_quantity
+   YEAR(order_date) AS years,
+   SUM(sales_amount) AS total_sales,
+   COUNT( DISTINCT customer_key) AS total_customer,
+   SUM(quantity) AS total_quantity
 FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY YEAR(order_date)
@@ -14,11 +14,11 @@ ORDER BY years ASC;
 
 -- Total Sales, Customer, Quantity for each Year and Month
 SELECT 
-YEAR(order_date) AS years,
-MONTH(order_date) months,
-SUM(sales_amount) AS total_sales,
-COUNT( DISTINCT customer_key) AS total_customer,
-SUM(quantity) AS total_quantity
+   YEAR(order_date) AS years,
+   MONTH(order_date) months,
+   SUM(sales_amount) AS total_sales,
+   COUNT( DISTINCT customer_key) AS total_customer,
+   SUM(quantity) AS total_quantity
 FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY YEAR(order_date), MONTH(order_date)
@@ -26,10 +26,10 @@ ORDER BY years ASC;
 
 -- Total Sales, Customer, Quantity by Month
 SELECT 
-MONTH(order_date) months, 
-SUM(sales_amount) AS total_sales,
-COUNT( DISTINCT customer_key) AS total_customer,
-SUM(quantity) AS total_quantity
+   MONTH(order_date) months, 
+   SUM(sales_amount) AS total_sales,
+   COUNT( DISTINCT customer_key) AS total_customer,
+   SUM(quantity) AS total_quantity
 FROM gold.fact_sales
 WHERE order_date IS NOT NULL
 GROUP BY  MONTH(order_date)
